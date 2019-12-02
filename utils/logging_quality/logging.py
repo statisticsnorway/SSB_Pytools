c62318e9e29c185a5bc2ad55d20a6de6038cec50
Svenkerud <ssv@ssb.no>
2019-12-02 13:48:56 +0100

    feat: Added the change detection function. added option to pring difference log to screan

--- utils/logging_quality/logging.py
+++ utils/logging_quality/logging.py
@@ -39,3 +39,30 @@ def ColumnComparison (data_set1, data_set2, output ='yes', log = None):
                 f.write('\n\nThese columns exist only in the second datasett:')
                 for elem in (b - a):
                     f.write('\n* %s'% elem)
+    elif log == None:
+        print('No Log created')
+
+    return
+
+def correctionDetection(start_data, comparison_data, log=None):
+    
+    if start_data.equals(comparison_data)==True:
+        print('No differneces detected, data considderes equal')
+    elif start_data.equals(comparison_data)==False:
+        print('Differences detected')
+        diff = start_data != comparison_data
+        for col in comparison_data.columns:
+            changes = comparison_data.loc[diff[col]]
+            original = start_data.loc[diff[col]]
+            if len(changes) > 1:
+                print('Column ',str(col),' is changed!')
+                for i in changes.reset_index().index:
+                    print('\nChange nr: ',i)
+                    print('Changes: ',changes.iloc[[i]].to_string(header=False))
+                    print('Original: ',original.iloc[[i]].to_string(header=False))
+        
+        
+    if log != None:
+        with open(log+timestr+'.log', 'w') as f:
+            if start_data.equals(comparison_data)==True:
+                f.write('No differneces detected, data considderes equal')
