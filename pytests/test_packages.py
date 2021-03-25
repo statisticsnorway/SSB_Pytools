adeac8f10d718d097edb9f1a85d64ce5edbad491
Carl F. Corneil <carl.corneil@ssb.no>
2021-03-25 12:12:42 +0000

    function must have ( ) ?

--- pytests/test_packages.py
+++ pytests/test_packages.py
@@ -2,6 +2,6 @@ import pytest
 import pandas as pd
 import numpy as np
 
-def test_pandas_numpy_installed:
+def test_pandas_numpy_installed():
     df_test = pd.DataFrame(np.random.randint(0,100,size=(100, 4)), columns=list('ABCD'))
     assert len(df_test) > 0 & isinstance(df_test, pd.DataFrame)    
