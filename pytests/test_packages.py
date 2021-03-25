1a2a3fb09b315ff84fc1ff9dce68f27086329360
Carl F. Corneil <carl.corneil@ssb.no>
2021-03-25 11:56:26 +0000

    newline?

--- pytests/test_packages.py
+++ pytests/test_packages.py
@@ -4,4 +4,4 @@ import numpy as np
 
 def test_pandas_numpy_installed:
     df_test = pd.DataFrame(np.random.randint(0,100,size=(100, 4)), columns=list('ABCD'))
-    assert len(df_test) > 0 & isinstance(df_test, pd.DataFrame)
+    assert len(df_test) > 0 & isinstance(df_test, pd.DataFrame)    
